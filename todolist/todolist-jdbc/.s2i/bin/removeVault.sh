1c96351e7c530d773d7316b4c4d105ff85c00554
Wilson choi <wilsonchoi@Wilsons-MacBook-Pro.local>
2018-02-08 23:21:14 +0800

    add remove vault script

---
+++ todolist/todolist-jdbc/.s2i/bin/removeVault.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+while true
+do
+   sleep 10
+   echo "start to checking readiness"
+   curl --noproxy '*' -s -u dQjQX35g:5TsOmvm0 'http://localhost:8080/manager/jmxproxy/?get=Catalina%3Atype%3DServer&att=stateName' |grep -iq 'stateName *= *STARTED'
+   if [ $? == 0 ]
+   then
+     break;
+   fi
+done
+rm /opt/webserver/tomcat-vault/VAULT.dat
+echo "finish to remove vault"
